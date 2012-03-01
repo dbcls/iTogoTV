4d2fbe4795c41fe99db8d53902ec7eee6103dc2d
tazro inutano ohta <inutano@gmail.com>
2012-03-01 15:29:09 +0900

    fixed to use class method

--- lib/togotvcurated_parser.rb
+++ lib/togotvcurated_parser.rb
@@ -5,18 +5,16 @@ require "nokogiri"
 require "yaml"
 
 class MainParser
-  def initialize
-    @page = Nokogiri::HTML(open("http://togotv-curated.dbcls.jp"))
-  end
-  
-  def latest_movies
+  @@page = Nokogiri::HTML(open("http://togotv-curated.dbcls.jp"))
+
+  def self.latest_movies
     # return href of 10 latest movies
-    @page.css("tbody.row-hover tr").map{|tr| tr.css("a").attr("href").value.gsub(/\#.+$/,"") }[0..9]
+    @@page.css("tbody.row-hover tr").map{|tr| tr.css("a").attr("href").value.gsub(/\#.+$/,"") }[0..9]
   end
   
-  def categories
+  def self.categories
     # return href of each movie categories page
-    @page.css(".pic-7").map{|l| l.attr("href")}.delete_if{|l| l.include?("list")}
+    @@page.css(".pic-7").map{|l| l.attr("href")}.delete_if{|l| l.include?("list")}
   end
 end
 
@@ -54,31 +52,25 @@ end
 
 class RankingParser
   # return 10 entries each, [[togotv url, view count, (up|down|same|new)]..]
-  def initialize
-    @yaml = YAML::load(open("http://togotv.dbcls.jp/count.yaml"))
+  @@yaml = YAML::load(open("http://togotv.dbcls.jp/count.yaml"))
+  
+  def self.yesterday
+    @@yaml[:yesterday]
   end
   
-  def yesterday
-    @yaml[:yesterday]
+  def self.this_week
+    @@yaml[:week]
   end
   
-  def this_week
-    @yaml[:week]
+  def self.this_month
+    @@yaml[:this_month]
   end
   
-  def this_month
-    @yaml[:this_month]
+  def self.last_month
+    @@yaml[:last_month]
   end
   
-  def this_love
-    "has taken its toll on me"
-  end
-  
-  def last_month
-    @yaml[:last_month]
-  end
-  
-  def last_christmas
+  def self.last_christmas
     "I gave you my heart"
   end
 end
