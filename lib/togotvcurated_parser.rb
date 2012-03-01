452c526f9072641b6e1b337725ef96906fd1664e
tazro inutano ohta <inutano@gmail.com>
2012-03-01 12:21:45 +0900

    create RankingParser class

--- lib/togotvcurated_parser.rb
+++ lib/togotvcurated_parser.rb
@@ -2,18 +2,20 @@
 
 require "open-uri"
 require "nokogiri"
-require "ap"
+require "yaml"
 
 class MainParser
   def initialize
     @page = Nokogiri::HTML(open("http://togotv-curated.dbcls.jp"))
   end
   
-  def newvideos
+  def latest_movies
+    # return href of 10 latest movies
     @page.css("tbody.row-hover tr").map{|tr| tr.css("a").attr("href").value.gsub(/\#.+$/,"") }[0..9]
   end
   
   def categories
+    # return href of each movie categories page
     @page.css(".pic-7").map{|l| l.attr("href")}.delete_if{|l| l.include?("list")}
   end
 end
@@ -24,6 +26,7 @@ class CategoryParser
   end
   
   def subcategories
+    # return a hash, { href of subcategories, title of subcategories }
     subcat = {}
     @page.css("li.toc-level-3").each do |li|
       href = li.css("a").attr("href").value
@@ -34,6 +37,7 @@ class CategoryParser
   end
   
   def subcat_movielist(subcat_href)
+    # return a hash, { href of togotv page, title of the movie }
     index_removed = @page.css(".hentry ul")[1]
     target_subcat = index_removed.css("li").select{|li| li.css("a").first.attr("name") == subcat_href }.first
     movlist = {}
@@ -45,5 +49,36 @@ class CategoryParser
       end
     end
     movlist
+  end
+end
+
+class RankingParser
+  # return 10 entries each, [[togotv url, view count, (up|down|same|new)]..]
+  def initialize
+    @yaml = YAML::load(open("http://togotv.dbcls.jp/count.yaml"))
+  end
+  
+  def yesterday
+    @yaml[:yesterday]
+  end
+  
+  def this_week
+    @yaml[:week]
+  end
+  
+  def this_month
+    @yaml[:this_month]
+  end
+  
+  def this_love
+    "has taken its toll on me"
+  end
+  
+  def last_month
+    @yaml[:last_month]
+  end
+  
+  def last_christmas
+    "I gave you my heart"
   end
 end
