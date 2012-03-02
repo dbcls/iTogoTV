01a37c50c6b3a903f026a18075b1535b7081e398
tazro inutano ohta <inutano@gmail.com>
2012-03-02 19:26:18 +0900

    now testing, caught an error to parsing thumbnail img URL (again!)

--- lib/xml_generator.rb
+++ lib/xml_generator.rb
@@ -55,25 +55,57 @@ build = Nokogiri::XML::Builder.new(:encoding => 'utf-8') do |xml|
         xml.category {
           xml.category_url url
           catp = CategoryParser.new(url)
-          catp.subcategories.each_pair do |url, title|
-            xml.subcategory {
-              xml.subcategory_url url
-              xml.subcategory_title title
-              ap url
-              catp.subcat_movielist(url).each do |url|
+          subcat = catp.subcategories
+          if subcat.class == Hash
+=begin
+            subcat.each_pair do |url, title|
+              xml.subcategory {
+                xml.subcategory_url url
+                xml.subcategory_title title
                 ap url
-                xml.movie {
-                  ttv = TogoTVParser.new(url)
-                  xml.url url
-                  xml.mp4file_url ttv.mp4file_url
-                  xml.title ttv.title
-                  xml.tag ttv.tag
-                  xml.thumbnail ttv.thumbnail
-                  xml.date ttv.date
-                  xml.description ttv.text
-                }
-              end
-            }
+                catp.subcat_movielist(url).each do |url|
+                  ap url
+                  if url.length == 36
+                  xml.movie {
+                    ttv = TogoTVParser.new(url)
+                    xml.url url
+                    xml.mp4file_url ttv.mp4file_url
+                    xml.title ttv.title
+                    xml.tag ttv.tag
+                    xml.thumbnail ttv.thumbnail
+                    xml.date ttv.date
+                    xml.description ttv.text
+                  }
+                  else
+                    ap "pass"
+                  end
+                end
+              }
+            end
+=end
+          else
+            subcat.each do |arr|
+              xml.subcategory {
+                xml.subcategory_url arr.last
+                xml.subcategory_title arr.first.first
+                xml.subcategory_event arr.first.last
+                ap arr.last
+                catp.subcat_movielist_lecture(arr.last).each do |url|
+                  ap url
+                  xml.movie {
+                    ttv = TogoTVParser.new(url)
+                    xml.url url
+                    xml.mp4file_url ttv.mp4file_url
+                    xml.title ttv.title
+                    xml.tag ttv.tag
+                    xml.thumbnail ttv.thumbnail
+                    xml.date ttv.date
+                    xml.description ttv.text
+                  }
+                sleep 10
+                end
+              }
+            end
           end
         }
       end
