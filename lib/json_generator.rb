964da77374b02bdeb9b2f6f0debfaed87d9bf13f
tazro inutano ohta <inutano@gmail.com>
2012-03-29 16:55:15 +0900

    create json_generator.rb (has some bug)

--- lib/json_generator.rb
+++ lib/json_generator.rb
@@ -2,12 +2,15 @@
 
 require "./togotvcurated_parser"
 require "./togotv_parser"
-
+require "json"
+require "ap"
 
 @movie_desc = MainParser.movie_desc
 
+begin
+
 latest = { :latest => [] }
-MainParser.latest_movies.each do |url|
+  MainParser.latest_movies.each do |url|
   cont = {}
   ttv = TogoTVParser.new(url)
   cont[ :url ] = url
@@ -88,20 +91,38 @@ MainParser.rank( :last_month ).each do |arr|
 end
 
 categories = { :categories => [] }
-# categories = { :categories => [ { :caturl => "category_url(http..)", :subcat => { "subcat_url(http..)" => [contents]}, "category_url2(http..)" => []}]}
+# categories = { :categories => [ { :subcat_name => [cont, cont..] } ] }
 
 MainParser.categories.each do |url|
-  categories[ :categories ].push(
-    { :category_url => url,
-      :subcategories => {}
-    }
-  )
   catp = CategoryParser.new(url)
   subcat = catp.subcategories
+
   if subcat.class == Hash
+    subcat_set = {}
     subcat.each_pair do |suburl, title|
-      categories[ :categories ] = []
-      # kaiteru tochu
+      subcat_set[title] = []
+      catp.subcat_movielist(suburl).each do |togotvurl|
+        cont = {}
+        ap togotvurl
+        ttv = TogoTVParser.new(togotvurl)
+        cont[ :url ] = togotvurl
+        cont[ :mp4file_url ] = ttv.mp4file_url
+        cont[ :title ] = ttv.title
+        cont[ :tag ] = ttv.tag
+        cont[ :thumbnail ] = ttv.thumbnail
+        cont[ :date ] = ttv.date
+        cont[ :description ] = ttv.text(@movie_desc)
+        subcat_set[title] << cont
+        sleep 10
+      end
+      categories[ :categories ] << subcat_set
     end
   end
 end
+
+rescue Timeout::Error
+  retry
+
+end
+
+open("../data/togotv_#{Time.now.strftime("%Y%m%d_%H%M")}.json","w"){|f| JSON.dump([latest, ranking, categories], f)}
